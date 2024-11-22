0187bb63b49c27e2cbb466ce48b5a7a48a3aaf74
Dvrecky <hubert@DESKTOP-28MDB1H>
2024-11-22 16:08:27 +0100

    Zdefiniowanie ścieżek dla komponentów

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home/home.component';
+import { AddTransactionComponent } from './pages/transaction/add-transaction/add-transaction.component';
+import { AccountComponent } from './pages/account/account/account.component';
+// dodanie (importowanie) komponentów, których użyjemy do routingu
 
-export const routes: Routes = [];
+// zdefiniowanie komponentów jakich Angular ma użyć dla podanych ścieżek
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'transaction', component: AddTransactionComponent},
+    {path: 'accounts', component: AccountComponent},
+];
