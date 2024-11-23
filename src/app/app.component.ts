8dbee394c76a3784bc2ddc3674f95fad6b3469f8
JAKUB GERWEL <kuba.gerwel@gmail.com>
2024-11-23 18:57:34 +0100

    dodanie footera i headera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SizeNavBarComponent } from "./components/side-nav-bar/size-nav-bar.component";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SizeNavBarComponent, MatSidenavModule, HeaderComponent],
+  imports: [RouterOutlet, SizeNavBarComponent, MatSidenavModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
