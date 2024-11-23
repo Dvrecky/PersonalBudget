7f03d322e446fa2ab305957fae69da789c7a7954
Dvrecky <hubert@DESKTOP-28MDB1H>
2024-11-23 16:58:04 +0100

    Skończenie side-bara

--- src/app/components/side-nav-bar/size-nav-bar.component.ts
+++ src/app/components/side-nav-bar/size-nav-bar.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-size-nav-bar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, MatListModule, MatToolbarModule],
   templateUrl: './size-nav-bar.component.html',
   styleUrls: ['./size-nav-bar.component.css']
 })
