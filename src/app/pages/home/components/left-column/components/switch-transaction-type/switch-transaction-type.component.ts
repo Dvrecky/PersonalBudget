8b093092ee9f04e8293d5ae803783fec147dbedc
Dvrecky <hubert@DESKTOP-28MDB1H>
2024-11-26 11:15:06 +0100

    Dodanie komentarzy do kodu

--- src/app/pages/home/components/left-column/components/switch-transaction-type/switch-transaction-type.component.ts
+++ src/app/pages/home/components/left-column/components/switch-transaction-type/switch-transaction-type.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Output} from '@angular/core';
 import {MatRadioModule} from '@angular/material/radio';
 
-
 @Component({
   selector: 'app-switch-transaction-type',
   standalone: true,
@@ -11,11 +10,14 @@ import {MatRadioModule} from '@angular/material/radio';
 })
 export class SwitchTransactionTypeComponent{
 
+  // input jest nie potrzeby, bo nie otrzymuje żadnych danych do wyświetlenia, itp.
+  // ten komponent służy tylko do pobrania typu transakcji (i przekazania jej rodzicowi), które mają być wyświetlone w innych komponentach
   // służy do powiadomienia rodzica o zmianie typu wydatków ('expenses' albo 'incomes')
-  @Output() transactionTypeSwitch = new EventEmitter<'expenses' | 'incomes'>();
+  @Output() transactionTypeSwitch = new EventEmitter<'expenses' | 'incomes'>(); // przyjmuje tylko dwie wartości
 
+  // metoda jest wywoływana w momencie, zmiany w radio-buttons
   onTransactionTypeChange(value: 'expenses' | 'incomes') {
-    console.log("Chosen option: " , value);
-    this.transactionTypeSwitch.emit(value);
+    console.log("Chosen option: " , value); // logi pomocnicze
+    this.transactionTypeSwitch.emit(value); // emitowanie zdarzenia do rodzica (przekazuje zaznaczoną wartość)
   }
 }
