0d8f138eefa37a829c32e126460482f455f08e40
JAKUB GERWEL <kuba.gerwel@gmail.com>
2024-11-26 11:34:56 +0100

    dodanie historii na stronie glownej

--- src/app/pages/home/components/right-column/components/transaction-history/transaction-history.component.ts
+++ src/app/pages/home/components/right-column/components/transaction-history/transaction-history.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Account } from '../../../../../../models/account.model';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, NgFor, CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-transaction-history',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe,NgFor,CommonModule, MatListModule],
   templateUrl: './transaction-history.component.html',
   styleUrl: './transaction-history.component.css'
 })
@@ -19,6 +20,24 @@ export class TransactionHistoryComponent implements OnChanges{
       } else {
         console.log("Jebło");
       }
+  }
+
+   transactions = [
+      { date: '2024-11-25', description: 'Zakupy spożywcze', amount: 150.0, type : 'expense'},
+      { date: '2024-11-24', description: 'Wypłata', amount: 5000.0, type : 'income'},
+      { date: '2024-11-23', description: 'Przelew oo znajomego', amount: 200.0, type : 'income'},
+      { date: '2024-11-22', description: 'Opłata za rachunki', amount: 450.0, type : 'expense' },
+      { date: '2024-11-21', description: 'Obiad w restauracji', amount: 80.0, type : 'expense'},
+      { date: '2024-11-25', description: 'Zakupy spożywcze', amount: 150.0, type : 'expense'},
+      { date: '2024-11-24', description: 'Wypłata', amount: 5000.0, type : 'income'},
+      { date: '2024-11-23', description: 'Przelew do znajomego', amount: 200.0, type : 'expense' },
+      { date: '2024-11-22', description: 'Opłata za rachunki', amount: 450.0, type : 'expense' },
+      { date: '2024-11-21', description: 'Obiad w restauracji', amount: 80.0, type : 'expense' },
+    ];
+
+  filterBy(period: string) {
+    console.log(`Filtruj dla okresu: ${period}`);
+    // Logika filtrowania transakcji w zależności od wybranego okresu.
   }
 
 }
