1f817383fb6954a189fd9d0d612a52cd98e71d6e
Dvrecky <hubert@DESKTOP-28MDB1H>
2024-12-04 14:28:19 +0100

    Dodanie szablonu wykresu dla kategorii

--- src/app/pages/home/components/left-column/components/category-chart/category-chart.component.ts
+++ src/app/pages/home/components/left-column/components/category-chart/category-chart.component.ts
@@ -1,7 +1,10 @@
-import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { Component, Output, Input, EventEmitter, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { CategoryChart } from '../../../../../../models/categoryChart.model';
+
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-category-chart',
@@ -10,12 +13,42 @@ import { CategoryChart } from '../../../../../../models/categoryChart.model';
   templateUrl: './category-chart.component.html',
   styleUrl: './category-chart.component.css'
 })
-export class CategoryChartComponent {
+export class CategoryChartComponent implements OnInit{
 
   // gotowe dane do wyświetlenia wykresu, będą przekazywane z rodzica
   @Input() categoryChartData: CategoryChart | undefined;
   // służy do emitowania zdarzenia do rodzica, informując go o zmianie okresu według, którego ma być wyświetlany wykres
   @Output() periodChange = new EventEmitter<"year" | "month" | "week" | "day">();
+
+  dane = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  config: any = {
+    type: 'doughnut',
+    data: this.dane,
+  };
+
+  chart: any;
+
+
+  ngOnInit(): void {
+    this.chart = new Chart('CategoryChart', this.config);
+  }
 
   onPeriodChange(period: "year" | "month" | "week" | "day") {
       console.log(period);
