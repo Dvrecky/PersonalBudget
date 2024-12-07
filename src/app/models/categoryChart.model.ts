c526dfcaf6a0edfa50f94a07adf0b4197d90e885
Dvrecky <hubert@DESKTOP-28MDB1H>
2024-12-07 17:47:31 +0100

    Dodanie CategorySummary dla wyświetlania podsumowania finansowego dla kategorii

--- src/app/models/categoryChart.model.ts
+++ src/app/models/categoryChart.model.ts
@@ -1,5 +1,3 @@
-import { Category } from "./category.model";
-
 export interface CategoryChart {
     name: string;
     color: string;
--- src/app/pages/home/components/left-column/left-column.component.ts
+++ src/app/pages/home/components/left-column/left-column.component.ts
@@ -14,8 +14,8 @@ import { Category } from '../../../../models/category.model';
 import { TransactionService } from '../../../../services/transaction.service';
 
 import { Chart, registerables } from 'chart.js';
-import { transition } from '@angular/animations';
-import { withHttpTransferCacheOptions } from '@angular/platform-browser';
+import { CategoryChartDataStructure } from '../../../../models/CategoryChartDataStructure.model';
+
 Chart.register(...registerables);
 
 @Component({
@@ -36,18 +36,10 @@ export class LeftColumnComponent implements OnInit{
   // zmienna przechowuje obecnie wybrany typ transakcji
   selectedTransactionType: 'expense' | 'income' = 'expense';
 
+  // zmienna reprezentująca wykres
   categoryChartData: any |undefined;
 
-  // zmienne, które będą się zmieniać dla wykresu
-
-  // zmienna przechowuje nazwy kategorii
-  categoryNames: string[] = [];
-
-  // zmienna przechowuje całościową kwotę dla danej kategorii
-  amounts: number[] = [];
-
-  // zmienna przechowuje kolory dla kategorii
-  categoryColors: string[] = [];
+  categoryDataStructure: CategoryChartDataStructure | undefined;
 
   // dane do wykresu
   dane = {
@@ -78,90 +70,8 @@ export class LeftColumnComponent implements OnInit{
   // konfiguracja wykresu (wykorzystuje dane)
   config: any = {
     type: 'doughnut',
-    data: this.dane,
+    data: this.dane ,
   };
-
-  onChartDataChange(accId: number, transactionType: 'expense' | 'income') {
-
-    // wszystkie konta mają te same kategorie transakcji (dla wydatków i dla przychodów)
-    // więc można zwyczajnie pobrać wszystkie kategorie dla danego typu transakcji
-
-    // pobranie kategorii dla danego typu transakcji
-    const categories = this.categoryService.getAllCategories().filter((category) => category.type === transactionType);
-
-    // pobranie kolorów kategorii
-    this.categoryColors = categories.map( (category) => category.color );
-
-    // pobranie nazw kategorii
-    this.categoryNames = categories.map((category) => category.name);
-
-    // zmienna przechowuje dane do wyświetlenia na wykresie
-    const categoryChart: CategoryChart[] = [];
-
-    if(this.selectedAccountId === 0) {
-
-      // const allTransactions = this.transactionService.getTransactions().filter( (transaction) => transaction.type === transactionType );
-
-      for(const category of categories) {
-        const categoryTransactions = this.transactionService.getTransactions().filter(
-          (transaction) =>
-            transaction.categoryId === category.id &&
-            transaction.type === transactionType
-        );
-
-        const sum = categoryTransactions.reduce( (acc, transaction) => acc + transaction.amount, 0);
-
-        categoryChart.push({
-          name: category.name,
-          color: category.color,
-          sum: sum,
-        });
-      }
-
-      console.log(categoryChart);
-
-
-    } else {
-
-      // dla każdej kategorii pobierane są transkacje
-      for(const category of categories) {
-        const categoryTransactions = this.transactionService.getTransactions().filter(
-          (transaction) => 
-            transaction.categoryId === category.id &&
-            transaction.type === transactionType && 
-            transaction.accountId === accId
-        );
-
-        const sum = categoryTransactions.reduce( (acc, transaction) => acc + transaction.amount, 0);
-
-        categoryChart.push({
-          name: category.name,
-          color: category.color,
-          sum: sum,
-        });
-      }
-
-      console.log(categoryChart);
-
-      // const transactions = this.transactionService.getTransactions().filter((transaction) => transaction.accountId === accId && transaction.type === transactionType);
-      
-      // this.dane.labels = this.categoryNames;
-      // this.dane.datasets[0].backgroundColor = this.categoryColors;
-    }
-
-    this.dane.labels = categoryChart.map((summary) => summary.name);
-    this.dane.datasets[0].data = categoryChart.map((summary) => summary.sum);
-    this.dane.datasets[0].backgroundColor = categoryChart.map( (summary) => summary.color);
-    // console.log("Names: ", this.categoryNames);
-    // console.log("Colors: ", this.categoryColors);
-
-    if (this.categoryChartData) {
-      this.categoryChartData.update();
-    }
-  }
-  
-  constructor(private transactionService: TransactionService, private categoryService: CategoryService, private accountService: AccountService, private appStateService: AppStateService) {
-  }
 
   ngOnInit(): void {
     // this.loadAccounts();
@@ -182,6 +92,88 @@ export class LeftColumnComponent implements OnInit{
 
     this.onChartDataChange(this.selectedAccountId, this.selectedTransactionType);
   }
+
+  onChartDataChange(accId: number, transactionType: 'expense' | 'income') {
+
+    // wszystkie konta mają te same kategorie transakcji (dla wydatków i dla przychodów)
+    // więc można zwyczajnie pobrać wszystkie kategorie dla danego typu transakcji
+
+    // pobranie kategorii dla danego typu transakcji
+    const categories = this.categoryService.getAllCategories().filter((category) => category.type === transactionType);
+
+    // zmienna przechowuje dane do wyświetlenia na wykresie
+    //   export interface CategoryChart {
+    //     name: string;
+    //     color: string;
+    //     sum: number;
+    // }
+    const categoryChart: CategoryChart[] = [];
+
+    // jeśli została wybrana SUMA
+    if(this.selectedAccountId === 0) {
+
+      // dla każdej kategorii (danego typu: "expense" lub "income") pobierane są wszystkie transakcje danego typu ("expense" lub "income")
+      for(const category of categories) {
+        const categoryTransactions = this.transactionService.getTransactions().filter(
+          (transaction) =>
+            transaction.categoryId === category.id &&
+            transaction.type === transactionType
+        );
+
+        // następnie dla listy transakcji danej kategorii obliczana jest łączna suma
+        const sum = categoryTransactions.reduce( (acc, transaction) => acc + transaction.amount, 0);
+
+        // dodanie danych do wyświetlenia dla danej transakcji
+        categoryChart.push({
+          name: category.name,
+          color: category.color,
+          sum: sum,
+        });
+      }
+
+      console.log(categoryChart);
+
+    } else {
+
+      // dla danego konta
+      
+      for(const category of categories) {
+        const categoryTransactions = this.transactionService.getTransactions().filter(
+          (transaction) => 
+            transaction.categoryId === category.id &&
+            transaction.type === transactionType && 
+            transaction.accountId === accId
+        );
+
+        const sum = categoryTransactions.reduce( (acc, transaction) => acc + transaction.amount, 0);
+
+        categoryChart.push({
+          name: category.name,
+          color: category.color,
+          sum: sum,
+        });
+      }
+
+      console.log(categoryChart);
+    }
+
+    // przypisanie nazw kategorii z categoryChart
+    this.dane.labels = categoryChart.map((summary) => summary.name);
+    // przypisanie sumy dla danej kategorii z categoryChart
+    this.dane.datasets[0].data = categoryChart.map((summary) => summary.sum);
+    // przypisanie kolorów kategorii z categoryChart
+    this.dane.datasets[0].backgroundColor = categoryChart.map( (summary) => summary.color);
+
+    // aktualizacja wykresu
+    if (this.categoryChartData) {
+      this.categoryChartData.update();
+    }
+  }
+  
+  constructor(private transactionService: TransactionService, private categoryService: CategoryService, private accountService: AccountService, private appStateService: AppStateService) {
+  }
+
+
 
   onAccountChange(accountId: number) {
     this.selectedAccountId = accountId;
