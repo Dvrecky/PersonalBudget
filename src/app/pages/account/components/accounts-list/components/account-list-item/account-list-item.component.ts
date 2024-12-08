b2641eb0b21a0ceca375fe8f915cd9f8e6c361b1
Dvrecky <hubert@DESKTOP-28MDB1H>
2024-12-08 03:24:31 +0100

    Dodanie panelu menu w liście kont

--- src/app/pages/account/components/accounts-list/components/account-list-item/account-list-item.component.ts
+++ src/app/pages/account/components/accounts-list/components/account-list-item/account-list-item.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Account } from '../../../../../../models/account.model';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-account-list-item',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule, MatIconModule, MatButtonModule],
   templateUrl: './account-list-item.component.html',
   styleUrl: './account-list-item.component.css'
 })
