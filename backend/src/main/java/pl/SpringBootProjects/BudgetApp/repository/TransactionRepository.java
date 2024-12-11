f724cb0efd0be1d0265660e00c336bac65f690b6
JAKUB GERWEL <kuba.gerwel@gmail.com>
2024-12-11 15:47:17 +0100

    dodanie reozotorium transakcji

---
+++ backend/src/main/java/pl/SpringBootProjects/BudgetApp/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package pl.SpringBootProjects.BudgetApp.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.SpringBootProjects.BudgetApp.entity.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
