4ef334a9acacc23b7b5f7a9beb5862d1164ee6e5
JAKUB GERWEL <kuba.gerwel@gmail.com>
2025-01-06 22:31:39 +0100

    dodanie zmiany kategorii zaleznie od typu transakcji w formualrzu dodajacym nowe transakcje

--- frontend/src/app/pages/transaction/add-transaction/add-transaction.component.ts
+++ frontend/src/app/pages/transaction/add-transaction/add-transaction.component.ts
@@ -78,7 +78,7 @@ export class AddTransactionComponent implements OnInit {
     this.loadAccounts();
 
     this.transactionForm = this.fb.group({
-      type: ['', [Validators.required]],
+      type: ['expense', [Validators.required]],
       accountId: ['', [Validators.required]],
       amount: [0.01, [Validators.required, Validators.min(0.01)]],
       description: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('^[a-zA-z0-9 ]+$')]],
@@ -107,7 +107,7 @@ export class AddTransactionComponent implements OnInit {
         () => {
 
           this.transactionForm.reset({
-            type: 'Expense',
+            type: 'expense',
             accountId: '',
             amount: 0.01,
             description: '',
